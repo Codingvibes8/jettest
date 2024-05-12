a9d73705f1fa593ec2d9bd76ea3e3d049d62575c
adedayo-888 <dayoadeniji69@yahoo.com>
2024-05-12 06:07:28 +0100

    added header component

---
+++ app/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                Welcome To Us
+                Keep Notes
+            </h1>
+        </header>
+    )}
+export default Header;
