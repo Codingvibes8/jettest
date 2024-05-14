14c3ecb8568575596018f89a01cf78c5859efad8
adedayo-888 <dayoadeniji69@yahoo.com>
2024-05-14 16:16:33 +0100

    added header component and Hero section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 
 const HeroSection = ()=>
 {
+
     return(
         <section className="lg:py-16">
             <div className="grid grid-cols-1 sm:grid-cols-12">
@@ -14,27 +15,28 @@ const HeroSection = ()=>
                     transition={{ duration: 0.5 }}
                     className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
                 >
-                    <h1 className="text-yellow-300 mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-none font-extrabold">
+                    <h1 className="text-yellow-300 mb-4 text-4xl sm:text-5xl  lg:leading-none font-extrabold font-serif">
                     <span>
-                      Hello, I&apos;m Dayo,<br/>A Web Developer based in London,U.K
+                        Hello, I&apos;m Dayo,<br/>
+                        <span className='text-green-700 font-serif'>A Web Developer Based In London,</span>
+                        <br/>U.K.
                        </span>
                     </h1>
-                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                        voluptuous.
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl font-serif">
+                        I create digital reality from your ideas with future-proof technologies
                     </p>
                     <div>
                         <Link
                             href="/#contact"
-                            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-yellow-300 hover:bg-slate-200 text-black"
+                            className="font-serif px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-yellow-300 hover:bg-slate-200 text-black"
                         >
                             Hire Me
                         </Link>
                         <Link
                             href="/"
-                            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-yellow-100 hover:bg-purple-400 hover:text-amber-50 text-black mt-3"
+                            className="px-1 font-serif inline-block py-1 w-full sm:w-fit rounded-full bg-yellow-100 hover:bg-purple-400 hover:text-amber-50 text-black mt-3"
                         >
-              <span className="block bg-yellow-50 hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block font-serif bg-yellow-50 hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
                         </Link>
@@ -46,17 +48,19 @@ const HeroSection = ()=>
                     transition={{ duration: 0.5 }}
                     className="col-span-4 place-self-center mt-4 lg:mt-0"
                 >
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+
+                    <div className="rounded-full bg-[#181818] mb-6 w-full h-full lg:w-full lg:h-full relative">
                         <Image
                             src="/images/portfolioHero.jpg"
                             alt="hero image"
-                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            height={300}
-                            width={300}
+                            className=""
+                           width={400} height={400}
                         />
                     </div>
+
                 </motion.div>
             </div>
+            <hr className='mt-6 text-yellow-100'/>
         </section>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from './components/HeroSection'
+import AboutSection from "@/app/components/AboutSection";
 
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-[#121212]">
+      <main className="flex min-h-screen flex-col bg-blue-950">
           <div className='container mt-24 mx-auto px-12 py-4'>
               <HeroSection/>
+              <AboutSection/>
           </div>
 
 
